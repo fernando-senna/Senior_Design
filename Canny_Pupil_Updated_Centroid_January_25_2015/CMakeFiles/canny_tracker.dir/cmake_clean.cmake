4e307508caaa6818eb515c5f6080880cfc393bf1
FLSNSN22 <krishnamani.bhattarai@mavs.uta.edu>
2016-01-25 12:26:34 -0600

    [dynamic pupil center, size and angle]

---
+++ Canny_Pupil_Updated_Centroid_January_25_2015/CMakeFiles/canny_tracker.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/canny_tracker.dir/canny_main.cpp.o"
+  "CMakeFiles/canny_tracker.dir/PupilTracker.cpp.o"
+  "canny_tracker.pdb"
+  "canny_tracker"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/canny_tracker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
